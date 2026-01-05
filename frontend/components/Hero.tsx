d6e94830be97f5bfb903bfe7a0dffedd0504dcbd
mohamed zaher <mohamedzaher.dev@gmail.com>
2026-01-05 18:19:54 +0300

    Darken hero images more - increase overlay opacity to hide background images better

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -67,8 +67,8 @@ export default function Hero() {
             </SwiperSlide>
           ))}
         </Swiper>
-        <div className="absolute inset-0 bg-black/85" />
-        <div className="absolute inset-0 bg-gradient-to-br from-royal-blue/30 via-black/50 to-black/60" />
+        <div className="absolute inset-0 bg-black/90" />
+        <div className="absolute inset-0 bg-gradient-to-br from-royal-blue/40 via-black/70 to-black/80" />
       </div>
 
       {/* Animated background elements */}
